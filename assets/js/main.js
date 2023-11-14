c0562ebb620bee9d7b78ca95b167235df2d8e5df
Deivison Santos <deivisonddj3@gmail.com>
2023-11-14 11:20:27 -0300

    Adicionado Requisição da Pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,31 @@ const pokemons_card = document.getElementById('pokemons_card')
 const loadMore = document.getElementById('loadMore');
 const nextPage = document.getElementById('nextPage');
 const prevPage = document.getElementById('prevPage');
+const pokedex_search = document.getElementById('forms_pokedex');
 const limit = 9
 let offset = 0
 const MaxPokemons = 225
+
+pokedex_search.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const id = document.getElementById("pokemon_identify");
+    const pokemon_gif = document.getElementsByClassName("add_gif");
+    
+    get_Pokemon_Gif(id.value).then((response) => {
+        if(response)
+        {
+            pokemon_gif[0].src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${response.id}.gif`;
+            pokemon_gif[1].src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${response.id}.gif`;
+        }
+    })
+})
+
+async function get_Pokemon_Gif(id) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${id.toLowerCase()}`)
+                .then((response) => response.json())
+                .catch(error => console.log('Erro na Requisição / Pokemon Inválido'))
+}
 
 function Pokemon_2_Li(Pokemon_card){
     return ` 
@@ -41,19 +63,15 @@ function Load_Pokemons_New_Page(offset, limit) {
 }
 
 loadMore.addEventListener('click', () => {
-
-    if(get_Max_Pokemons() == true){
-        offset += limit
-        Load_pokemons(offset, limit)
-    }
+    offset += limit
+    Load_pokemons(offset, limit)
+    
 })
 
 nextPage.addEventListener('click', () => {
-
-    if(get_Max_Pokemons() == true){
-        offset += limit
-        Load_Pokemons_New_Page(offset, limit)
-    }
+    offset += limit
+    Load_Pokemons_New_Page(offset, limit)
+    
 })
 
 prevPage.addEventListener('click', () => {
@@ -63,6 +81,7 @@ prevPage.addEventListener('click', () => {
     }
 })
 
+//Limita os pokemons caso necessário
 function get_Max_Pokemons() {
     if(offset + (2*limit) <= MaxPokemons)
         return true
